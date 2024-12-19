631d452285756483f532884f410f0a20ef9aaa84
Carlos Espinoza <carlos.espinoza23@unmsm.edu.pe>
2024-12-18 21:58:05 -0500

    init

---
+++ src/main/java/com/elranchoabelito/mcproductos/McProductosApplication.java
@@ -0,0 +1,13 @@
+package com.elranchoabelito.mcproductos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McProductosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McProductosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elranchoabelito/mcproductos/McProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elranchoabelito.mcproductos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McProductosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
